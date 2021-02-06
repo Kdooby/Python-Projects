a1247e45c2c785ff16c60b4fbc29d9b2ea026220
Kdooby <68250481+Kdooby@users.noreply.github.com>
2021-02-05 16:54:52 -0800

    Delete Django/DjangoUniversity directory

--- Django/DjangoUniversity/Classes/classApp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import djangoClasses
-
-
-# Register your models here.
-# registers the class in the admin section of the website
-admin.site.register(djangoClasses)
